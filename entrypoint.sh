32107e06a770705d92f2c6256894016bc4a7c120
checkago <mfalcon@mail.ru>
2021-10-13 22:10:48 +0300

    docker work!!!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 
-python manage.py runserver
+gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
