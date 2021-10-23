d4a58c438a042c619e91e5a932739cbfdae46ab4
Eugene Checkoy <mfalcon@mail.ru>
2021-10-23 13:51:30 +0300

    update contact_page & resources_projects

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-# import environ
-#
-# env = environ.Env()
-# environ.Env.read_env('.env')
+import environ
+
+env = environ.Env()
+environ.Env.read_env('.env')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
