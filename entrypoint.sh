32f26e49c2c1d7b0ff54e3783450ad29a89f95ab
Eugene Checkoy <mfalcon@mail.ru>
2021-10-23 14:37:37 +0300

    update contact_page & resources_projects

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 
 python manage.py migrate --run-syncdb
-python manage.py loaddata datadump.json
+python manage.py loaddata db.json
 #python manage.py collectstatic --no-input
 
 gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
