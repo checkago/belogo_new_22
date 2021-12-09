beebcc3fa6f4df12ea639ffda1eae0154334c180
Eugene Checkoy <mfalcon@mail.ru>
2021-12-09 09:42:04 +0300

    allowed host

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qgj-94qz%*=o+4byxx%##qoy*wx$luxs0%_rp$#!i1_!*%)9jt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['biblioteka-belogo.ru', '213.141.153.187', '127.0.0.1']
 
 
 # Application definition
