b472f65fe43c904dd134f832d92a0bcf1f05be5f
Eugene Checkoy <mfalcon@mail.ru>
2022-02-08 09:23:36 +0300

    robots.txt

--- belogo_new/urls.py
+++ belogo_new/urls.py
@@ -3,11 +3,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
 
 if settings.DEBUG:
