9c9b9533fcc54725be10f99c23d95f7b8835e96d
Eugene Checkoy <mfalcon@mail.ru>
2022-06-14 14:20:22 +0300

    debug false

--- belogo_new/urls.py
+++ belogo_new/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),),
 ]
 
-if settings.DEBUG:
-    if settings.MEDIA_ROOT:
-        urlpatterns += static(settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT)
-        urlpatterns += staticfiles_urlpatterns()
+if settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
--- web/urls.py
+++ web/urls.py
@@ -39,8 +39,7 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    if settings.MEDIA_ROOT:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-        urlpatterns += staticfiles_urlpatterns()
+if settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
