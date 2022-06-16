9bebb14b02854b540f327a69df0bde80de7825d2
Eugene Checkoy <mfalcon@mail.ru>
2022-06-16 09:10:15 +0300

    contact form domen dk denied

--- web/forms.py
+++ web/forms.py
@@ -28,14 +28,29 @@ class QuestionForm(forms.ModelForm):
 
 
 class FeedbackForm(forms.ModelForm):
-    comment = forms.CharField(
+    text = forms.CharField(
         label='Сообщение',
         widget=forms.Textarea(attrs={'rows': '1'})
     )
+    email = forms.EmailField(required=False, widget=forms.EmailInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = 'Имя'
+        self.fields['phone'].label = 'Номер телефона'
+        self.fields['email'].label = 'E-mail'
+        self.fields['comment'].label = 'Коментарий'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net', 'org', 'xyz', 'de', 'fr', 'ua', 'nl', 'cz', 'group', 'biz', 'dk']:
+            raise forms.ValidationError(f'Использование почтового ящика в домене .{domain} заблокированно')
+        return email
 
     class Meta:
         model = Feedback
-        fields = ('name', 'phone', 'email', 'comment', 'category')
+        fields = ('name', 'phone', 'email', 'comment')
 
 
 class ServiceDopForm(forms.ModelForm):
