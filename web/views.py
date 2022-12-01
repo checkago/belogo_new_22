891c2c76aa3de03a96ade6f7184048b9b8f1adcf
Eugene Checkoy <mfalcon@mail.ru>
2022-12-01 10:56:50 +0300

    page count in news

--- web/views.py
+++ web/views.py
@@ -50,7 +50,11 @@ def news(request):
     branch_categories = categories.get_descendants(include_self=True)
     news_list = News.objects.filter(category__in=branch_categories).distinct().order_by('-date')
     paginator = Paginator(news_list, 10, allow_empty_first_page=False)
-    page_number = request.GET.get('page')
+    try:
+        page_number = request.GET.get('page')
+    except PageNotAnInteger:
+        page_number = paginator.page(1)
+
     page_obj = paginator.get_page(page_number)
     page_num_list = paginator.get_elided_page_range(page_number, on_each_side=3)
 
