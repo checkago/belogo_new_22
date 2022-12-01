54ee02cc2dc4ff63e485633ab3de5b849d275243
Eugene Checkoy <mfalcon@mail.ru>
2022-12-01 10:43:55 +0300

    page count in news

--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, PageNotAnInteger
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.mail import send_mail
 from .forms import *
 from .models import *
@@ -50,12 +50,15 @@ def news(request):
     branch_categories = categories.get_descendants(include_self=True)
     news_list = News.objects.filter(category__in=branch_categories).distinct().order_by('-date')
     paginator = Paginator(news_list, 10)
+    page_number = request.GET.get('page')
     try:
-        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
-        page_number = request.GET.get(1)
-    page_obj = paginator.get_page(page_number)
+        page_obj = paginator.get_page(1)
+    except EmptyPage:
+        # If page is not an integer, deliver first page.
+        page_obj = paginator.get_page(1)
     page_num_list = paginator.get_elided_page_range(page_number, on_each_side=3)
 
     return render(request, 'news_all.html', {'categories': categories, 'title': title, 'news_list': news_list,
