afd092cf4d34935acdab1df4f0917f78309a0f7f
Eugene Checkoy <mfalcon@mail.ru>
2022-12-09 13:29:24 +0300

    some change in views

--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator
 from django.core.mail import send_mail
 from .forms import *
 from .models import *
