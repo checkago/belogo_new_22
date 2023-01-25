ac8ba4ff80c12e8553c8021b59b2bae8a0441f40
Eugene Checkoy <mfalcon@mail.ru>
2023-01-25 10:30:43 +0300

    forms, settings and views count

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -89,23 +89,24 @@ WSGI_APPLICATION = 'belogo_new.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("POSTGRES_DB"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
-        'HOST': env("POSTGRES_HOST"),
-        'PORT': env("POSTGRES_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("POSTGRES_DB"),
+#         'USER': env("POSTGRES_USER"),
+#         'PASSWORD': env("POSTGRES_PASSWORD"),
+#         'HOST': env("POSTGRES_HOST"),
+#         'PORT': env("POSTGRES_PORT"),
+#         'CONN_MAX_AGE': 60 * 10,  # 10 minutes
+#     }
+# }
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
