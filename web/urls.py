22164fde903850cb2d4cec28ddd6617ec8d0c6d3
Eugene Checkoy <mfalcon@mail.ru>
2023-04-21 15:15:57 +0300

    allow POST to Books

--- web/urls.py
+++ web/urls.py
@@ -3,8 +3,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.urls import path
 from . import views
-from .views import BibliotekiAPIView, NewsAPIView, EventAPIView, SheduleAPIView, ServiceAPIView, FreeServiceAPIView, \
-    BookFormsAPIView
+from .views import BibliotekiAPIView, NewsAPIView, EventAPIView, SheduleAPIView, ServiceAPIView, BookFormsAPIView
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -48,7 +47,6 @@ urlpatterns = [
     path('api/v1/event_list/', EventAPIView.as_view()),
     path('api/v1/shedule_list/', SheduleAPIView.as_view()),
     path('api/v1/services_list/', ServiceAPIView.as_view()),
-    path('api/v1/freeservices_list/', FreeServiceAPIView.as_view()),
     path('api/v1/book_forms/', BookFormsAPIView.as_view())
 ]
 
