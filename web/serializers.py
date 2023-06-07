b51431d744e2ed83770d1f6f915aa42afa88e811
Eugene Checkoy <mfalcon@mail.ru>
2023-06-07 13:30:38 +0300

    views serializers order update

--- web/serializers.py
+++ web/serializers.py
@@ -65,7 +65,7 @@ class DayEventSerializer(serializers.ModelSerializer):
 
 class SheduleDaySerializer(serializers.ModelSerializer): 
     events_list = DayEventSerializer(many=True)
-    date = serializers.DateField(format='%d %B')
+    date = serializers.DateField(format='%d.%E.%Y')
 
     class Meta: 
         model = SheduleDay 
