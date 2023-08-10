c8aa96b10b896f50ec64adc51a021277871d8b39
Eugene Checkoy <mfalcon@mail.ru>
2023-08-10 12:41:26 +0300

    установка редис, селери. автообновление поля active в week

--- web/tasks.py
+++ web/tasks.py
@@ -1,10 +1,13 @@
 from celery import shared_task
 from django.utils import timezone
-from .models import Week
+from .models import Week, WeekCDSCH, WeekBER, WeekF2, WeekF3, WeekF4
+
 
 @shared_task
 def update_active_field():
     today = timezone.now().date()
-    weeks = Week.objects.filter(start_date__lte=today, end_date__gte=today)
-    weeks.update(active=True)
-    weeks.exclude(active=True).update(active=False)
+    models = [Week, WeekCDSCH, WeekBER, WeekF2, WeekF3, WeekF4]  # Список ваших моделей
+    for model in models:
+        weeks = model.objects.filter(start_date__lte=today, end_date__gte=today)
+        weeks.update(active=True)
+        weeks.exclude(active=True).update(active=False)
