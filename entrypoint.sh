f128f8d67d8314974269d74b975514803e75c566
Eugene Checkoy <mfalcon@mail.ru>
2023-08-11 16:43:33 +0300

    flower for celery

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py migrate --run-syncdb
 python manage.py loaddata dbdump.json
 #python manage.py collectstatic --no-input
 
-gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8000 --reload  -w 4
+gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8080 --reload  -w 4
