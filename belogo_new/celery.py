3edb94b8dec7e9bb112749bec09fa5e3a56cf0f0
Eugene Checkoy <mfalcon@mail.ru>
2023-08-11 15:47:52 +0300

    flower for celery

--- belogo_new/celery.py
+++ belogo_new/celery.py
@@ -1,7 +1,17 @@
 import os
+from datetime import timedelta
+
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belogo_new.settings")
 app = Celery("belogo_new")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.beat_schedule = {
+    'update-cinema-week-active': {
+        'task': 'myapp.tasks.update_cinema_week_active',
+        'schedule': timedelta(hours=1),
+    },
+}
+
 app.autodiscover_tasks()
