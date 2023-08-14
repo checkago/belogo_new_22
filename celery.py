178af0b330c383180f5eb8fd181006b9fdeb056f
Eugene Checkoy <mfalcon@mail.ru>
2023-08-14 16:15:41 +0300

    flower for celery

--- celery.py
+++ celery.py
@@ -7,11 +7,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "belogo_new.settings")
 app = Celery("belogo_new")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+app.autodiscover_tasks()
+
 app.conf.beat_schedule = {
     'update-cinema-week-active': {
         'task': 'myapp.tasks.update_cinema_week_active',
         'schedule': timedelta(hours=1),
     },
 }
-
-app.autodiscover_tasks()
