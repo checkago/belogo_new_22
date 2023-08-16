124594fcae0943967d520f59f8ce9128f995844a
Eugene Checkoy <mfalcon@mail.ru>
2023-08-16 10:39:39 +0300

    flower for celery

--- belogo_new/celery.py
+++ belogo_new/celery.py
@@ -1,11 +1,12 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'belogo_new.settings')
 
 app = Celery('belogo_new')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 @app.task(bind=True)
 def debug_task(self):
