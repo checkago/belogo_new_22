e09171e8ca2d97bd7c2dfb5c71322ba80d723a59
Eugene Checkoy <mfalcon@mail.ru>
2023-08-16 13:54:26 +0300

    flower for celery

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -14,7 +14,6 @@ import os
 import environ
 from pathlib import Path
 
-from datetime import time
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
