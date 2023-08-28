11a878f9641a8260540a61ec4cef9aa76c538b67
Eugene Checkoy <mfalcon@mail.ru>
2023-08-28 10:06:01 +0300

    schedules cinema

--- belogo_new/celery.py
+++ belogo_new/celery.py
@@ -12,30 +12,30 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.beat_schedule = {
     'update-cinema-week-status': {
         'task': 'web.tasks.update_cinema_week_status',
-        'schedule': crontab(minute=1, hour=0),  # change to `crontab(minute=0, hour=0)` if you want it to run daily at midnight
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-week-status': {
         'task': 'web.tasks.update_week_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-weekcdsch-status': {
         'task': 'web.tasks.update_weekcdsch_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-weekber-status': {
         'task': 'web.tasks.update_weekber_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-weekf2-status': {
         'task': 'web.tasks.update_weekf2_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-weekf3-status': {
         'task': 'web.tasks.update_weekf3_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
     'update-weekf4-status': {
         'task': 'web.tasks.update_weekf4_status',
-        'schedule': crontab(minute=1, hour=0),
+        'schedule': crontab(minute=0),  # изменено на каждый час
     },
 }
