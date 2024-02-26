462b39791de078bfabd78e4ee8f525bd4ec69c99
Eugene Checkoy <mfalcon@mail.ru>
2024-02-26 13:42:16 +0300

    SSL for Nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Подготовка и установка SSL-сертификата с помощью Certbot
+certbot certonly --nginx --non-interactive --agree-tos --email mfalcon@mail.ru -d obs-balashiha.ru -d www.obs-balashiha.ru
 
+# Перезапуск Nginx для применения изменений
+service nginx restart
+
+# Выполнение миграций и других необходимых команд
 python manage.py migrate --run-syncdb
 python manage.py loaddata dbdump.json
 #python manage.py collectstatic --no-input
-
-gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8080 --reload  -w 4
+gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8080 --reload -w 4
