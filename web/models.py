22f6912fecab821ed570d209baff4cd65cb0bab3
Eugene Checkoy <mfalcon@mail.ru>
2024-03-26 16:34:21 +0300

    calendar

--- web/models.py
+++ web/models.py
@@ -185,7 +185,7 @@ class Event(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название события')
     library = models.CharField(max_length=150, choices=BIB_CHOICES, default=IKC,
                                verbose_name='Выбор Библиотеки')
-    date = models.DateTimeField(default=date.today, verbose_name='Дата проведения')
+    date = models.DateTimeField(verbose_name='Дата проведения')
     description = models.TextField(blank=True, verbose_name='Описание')
     image = models.ImageField(upload_to='img/events', blank=True, null=True, verbose_name='Изображение')
 
