0444def842beb21470a111801ee0423fce9458af
Eugene Checkoy <mfalcon@mail.ru>
2024-04-09 16:17:35 +0300

    news library

--- web/models.py
+++ web/models.py
@@ -106,6 +106,7 @@ class ImageGallery(models.Model):
 class News(models.Model):
     date = models.DateField(default=date.today, verbose_name='Дата')
     name = models.CharField(max_length=250, verbose_name='Заголовок', db_index=True)
+    library = models.ForeignKey(Biblioteka, blank=True, on_delete=models.CASCADE, null=True, verbose_name='Библиотека')
     category = models.ForeignKey(Category, blank=True, on_delete=models.SET_NULL, related_name='news', null=True,
                                  verbose_name='Категория', db_index=True)
     description = models.TextField(verbose_name='Текст')
@@ -776,13 +777,13 @@ class Eventy(models.Model):
     )
     """ПОЛЯ age, day, start и end доджны быть blank=True и null=True при следующих миграциях"""
 
-    day = models.ForeignKey(Day, on_delete=models.CASCADE, verbose_name='День недели', related_name='events')
+    day = models.ForeignKey(Day,blank=True, null=True,  on_delete=models.CASCADE, verbose_name='День недели', related_name='events')
     name = models.CharField(max_length=150, verbose_name='Название')
     payment = models.BooleanField(default=False, verbose_name='Платное')
     booking = models.BooleanField(default=False, verbose_name='Запись')
-    age = models.CharField(max_length=50, choices=AGE_CHOICES, default=ZERO, verbose_name='Возраст')
-    start_time = models.TimeField(verbose_name='Время начала')
-    end_time = models.TimeField(verbose_name='Время окончания')
+    age = models.CharField(max_length=50,blank=True, null=True,  choices=AGE_CHOICES, default=ZERO, verbose_name='Возраст')
+    start_time = models.TimeField(blank=True, null=True, verbose_name='Время начала')
+    end_time = models.TimeField(blank=True, null=True, verbose_name='Время окончания')
 
     class Meta:
         verbose_name = 'Мероприятие ИКЦ'
