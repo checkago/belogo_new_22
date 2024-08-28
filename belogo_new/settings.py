87a9a8e41c43c6cef92b264259dc99a6d4d25b14
Eugene Checkoy <mfalcon@mail.ru>
2024-08-28 16:54:17 +0300

    правка раздела документы

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -290,7 +290,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     (BASE_DIR / 'static_dev'),
