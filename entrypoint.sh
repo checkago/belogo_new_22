092798779f45803ee29b525baa28fc044cfe33f5
Eugene Checkoy <mfalcon@mail.ru>
2024-11-18 21:40:19 +0300

    SSL cert

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 # Выполнение миграций и других необходимых команд
 python manage.py migrate --run-syncdb
-python manage.py loaddata dbdump.json
+#python manage.py loaddata dbdump.json
 #python manage.py collectstatic --no-input
 gunicorn belogo_new.wsgi:application --bind 0.0.0.0:8080 --reload -w 4
