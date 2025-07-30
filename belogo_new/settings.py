f80cda3f663486078ede67ba808f7e233ee68cac
Eugene Checkoy <mfalcon@mail.ru>
2025-07-30 10:46:29 +0300

    versia dlya slabovideashih

--- belogo_new/settings.py
+++ belogo_new/settings.py
@@ -197,24 +197,24 @@ ADMIN_REORDER = (
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 20MB
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env("POSTGRES_DB"),
-#         'USER': env("POSTGRES_USER"),
-#         'PASSWORD': env("POSTGRES_PASSWORD"),
-#         'HOST': env("POSTGRES_HOST"),
-#         'PORT': env("POSTGRES_PORT"),
-#         'CONN_MAX_AGE': 60 * 10,  # 10 minutes
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("POSTGRES_DB"),
+        'USER': env("POSTGRES_USER"),
+        'PASSWORD': env("POSTGRES_PASSWORD"),
+        'HOST': env("POSTGRES_HOST"),
+        'PORT': env("POSTGRES_PORT"),
+        'CONN_MAX_AGE': 60 * 10,  # 10 minutes
+    }
+}
 
 
 # caches = {
